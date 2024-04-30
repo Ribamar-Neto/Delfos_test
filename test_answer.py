f9d52eabf10d05c8ec77728847981f8956ffcd69
Ribamar-Neto <114532399+Ribamar-Neto@users.noreply.github.com>
2024-04-29 22:19:01 -0300

    Update test_answer.py

--- test_answer.py
+++ test_answer.py
@@ -7,7 +7,9 @@ df = pd.DataFrame(sns.load_dataset('flights'))
 print(df.loc[df["passengers"].idxmax()])
 # Other way to do it, but without the methods loc() and idxmax().
 months_passengers = {}
+passengers = df['passengers']
+months = df['month']
 for i in range(len(df)):
-  months_passengers[df['passengers'][i]] = df['month'][i]
-print(f'\nMonth: {months_passengers[max(df["passengers"])]}'
-f'\nPassengers: {max(df["passengers"])}')
+  months_passengers[passengers[i]] = months[i]
+print(f'\nMonth: {months_passengers[max(passengers)]}'
+f'\nPassengers: {max(passengers)}')
