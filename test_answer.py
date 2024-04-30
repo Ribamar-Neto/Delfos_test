ad0a29bc8cb6fc8861a08d6dea8b63193f05127b
Ribamar-Neto <114532399+Ribamar-Neto@users.noreply.github.com>
2024-04-29 21:41:30 -0300

    Add files via upload

---
+++ test_answer.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import pandas as pd
+
+# loc: "localizes" a label in the DataFrame.
+# idxmax: takes the index of the occurrence of the maximum value of the group (in the case, df['passengers'])
+df = pd.DataFrame(sns.load_dataset('flights'))
+print(df.loc[df["passengers"].idxmax()])
+# Other way to do it, but without the methods loc() and idxmax().
+months_passengers = {}
+for i in range(len(df)):
+  months_passengers[df['passengers'][i]] = df['month'][i]
+print(f'\nMonth: {months_passengers[max(df["passengers"])]}'
+f'\nPassengers: {max(df["passengers"])}')
